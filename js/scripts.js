34f1ed486dc5f4f202ec3e28f2c612396e593033
Molly McGlone <mollykmcglone@gmail.com>
2016-03-04 14:14:24 -0800

    update scripts.js to actually work!

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
   $("#translate form").submit(function(event) {
-
-    var entry = $( "input" ).val();
-    console.log(entry);
+    event.preventDefault();
+    var $entry = $( "input" ).val().toLowerCase();
+    console.log($entry);
 
     var PigLatin = function(word) {
+      var translatedWord = word;
       if (word[0] === "a" || word[0] === "e" || word[0]=== "i" || word[0] === "o" || word[0] === "u") {
-        return word + "ay";
+        var translatedWord = word;
+      } else if (word[0] === "q"){
+        var translatedWord = word.slice(2) + word[0] + word[1];
+      } else if (word[1] === "q"){
+        var translatedWord = word.slice(3) + word[0] +word[1] + word[2];
+      } else if (word[1] === "a" || word[1] === "e" || word[1]=== "i" || word[1] === "o" || word[1] === "u") {
+        var translatedWord = word.slice(1) + word[0];
+      } else if (word[2] === "a" || word[2] === "e" || word[2]=== "i" || word[2] === "o" || word[2] === "u") {
+        var translatedWord = word.slice(2) + word[0] + word[1];
+      } else if (word[3] === "a" || word[3] === "e" || word[3]=== "i" || word[3] === "o" || word[3] === "u") {
+          var translatedWord = word.slice(3) + word[0] + word[1] + word[2];
       }
-      // } else {
-      //   var translatedWord = word.slice(1) + word(0) + "ay";
-      // }
-      // return translatedWord + "ay";
+      return translatedWord + "ay";
     };
 
-    PigLatin(entry);
+    console.log(PigLatin($entry));
 
-    event.preventDefault();
+
     });
   });
