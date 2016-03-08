0a10fbee05d8c0ca1a552d1844853f0fa4d69861
Molly McGlone <mollykmcglone@gmail.com>
2016-03-07 21:22:19 -0800

    add initial files

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,19 @@
-$(document).ready(function() {
-  $("#translate form").submit(function(event) {
-    event.preventDefault();
-    var $entry = $( "input" ).val().toLowerCase();
-    var Answer = "";
 
-    function print(Answer) {
-      var Resultdiv = document.getElementById("result");
-      Resultdiv.innerHTML = "<h2>Your Result is:</h2>" +Answer;
-    }
 
-    var PigLatin = function(word) {
-      var translatedWord = word;
-      if (word[0] === "a" || word[0] === "e" || word[0]=== "i" || word[0] === "o" || word[0] === "u") {
-        var translatedWord = word;
-      } else if (word[0] === "q"){
-        var translatedWord = word.slice(2) + word[0] + word[1];
-      } else if (word[1] === "q"){
-        var translatedWord = word.slice(3) + word[0] +word[1] + word[2];
-      } else if (word[1] === "a" || word[1] === "e" || word[1]=== "i" || word[1] === "o" || word[1] === "u") {
-        var translatedWord = word.slice(1) + word[0];
-      } else if (word[2] === "a" || word[2] === "e" || word[2]=== "i" || word[2] === "o" || word[2] === "u") {
-        var translatedWord = word.slice(2) + word[0] + word[1];
-      } else if (word[3] === "a" || word[3] === "e" || word[3]=== "i" || word[3] === "o" || word[3] === "u") {
-          var translatedWord = word.slice(3) + word[0] + word[1] + word[2];
+
+    var romanNumerals = function(number) {
+      var romanOnes = [];
+      for (i = 0; i < number; i ++) {
+        if (number >= 5) {
+          romanOnes.push("V");
+          number = (number % 5) + 1;
+        } else if (number === 4) {
+          romanOnes.push("IV");
+          number = 0;
+        } else if (number <= 3) {
+          romanOnes.push("I")
+        }
       }
-      return translatedWord + "ay";
+
+      return romanOnes.join("");
     };
-
-    var Answer = (PigLatin($entry));
-    print(Answer);
-
-    });
-  });
